0e30a6179c30c94a1fd5638d9ffb080e8a94c8ed
ahernan79 <58749534+ahernan79@users.noreply.github.com>
2020-02-03 15:46:52 -0500

    Update Hellofunctions.ino

--- NotSoBasicArduino/Hellofunctions/Hellofunctions.ino
+++ NotSoBasicArduino/Hellofunctions/Hellofunctions.ino
@@ -13,17 +13,17 @@ void setup()
 }
 void loop()
 {
- Serial.print("distance:");
- Serial.println(distance);
- getDistance();
- myservo.write(78);
-  if (distance <= 10)
+ Serial.print("distance:"); // prints word "distance" to serial
+ Serial.println(distance); //prints distance value returning from sensor
+ getDistance(); //gets value returning from sensor
+ myservo.write(78); //moves servo to what looks like 0 with the actual servo
+  if (distance <= 10) //if distance returning from sensor is less or equal to ten, move servo 180 degrees
   {
    myservo.write(180);
   }
  
 }
-int getDistance()
+int getDistance() //codes for above use of getDistance function which gets data from the sensor
 {
  digitalWrite(trigPin, LOW);
  delayMicroseconds(2);
