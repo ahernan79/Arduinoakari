85289e9c72bd6cea7aa56a62dfd2cb8fd0d2e560
ahernan79 <58749534+ahernan79@users.noreply.github.com>
2020-02-04 14:46:50 -0500

    Update Motor Control.ino

--- IntermediateArduino/Motor Control/Motor Control.ino
+++ IntermediateArduino/Motor Control/Motor Control.ino
@@ -1,7 +1,6 @@
-const int dialpin = A5;
-const int diodepin = A4;
+const int dialpin = A5; //potentiometer data pin
+const int diodepin = A4; //diode data pin
 int value;
-int speed;
 void setup()
 {
 	pinMode(dialpin, INPUT);
@@ -12,8 +11,7 @@ void setup()
 void loop()
 {
  value = analogRead(dialpin);      //Read and save analog value from potentiometer
- //speed = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-  analogWrite(diodepin, value);      	//Send PWM value to led
+  analogWrite(diodepin, value);      	//Sends potentiometer value to motor
   delay(100);	
 
 	delay(500);	
