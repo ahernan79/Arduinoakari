fdda02f478b48e7729f3a2bf9a50dbea805cd87f
ahernan79 <58749534+ahernan79@users.noreply.github.com>
2020-02-04 14:44:57 -0500

    Update Potentiometers.ino

--- IntermediateArduino/Potentiometers/Potentiometers.ino
+++ IntermediateArduino/Potentiometers/Potentiometers.ino
@@ -1,7 +1,7 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <Wire.h> //includes potentiometer library
+#include <LiquidCrystal_I2C.h> //includes lcd and lcd backpack library
 LiquidCrystal_I2C lcd(0x3F,16,2);
-const int bluePin = A0;
+const int bluePin = A0; //potentiometer pin
 const int ledPin = 6;
 int bright;
 int display;
@@ -20,9 +20,9 @@ void loop()
   lcd.setCursor(0, 1);
  value = analogRead(bluePin);      //Read and save analog value from potentiometer
  bright = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-  analogWrite(ledPin, bright);      	//Send PWM value to led
+  analogWrite(ledPin, bright);      	//Sends ptentiometer value to led 
   delay(100);	
- display = map(value, 0, 1023, 10, 0);
+ display = map(value, 0, 1023, 10, 0); //turns potentiometer value into a value between 1 and 10 and prints to lcd
 	lcd.print(display);
 	delay(500);
 }   
